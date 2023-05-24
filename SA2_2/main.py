e7bc600c5d27e76b3721e7e58ecf9813fa3d3e56
vishalgddam873 <vishal18@navgurukul.org>
2023-05-24 10:49:57 +0530

    Intial comment:

---
+++ SA2_2/main.py
@@ -0,0 +1,27 @@
+# Importing Library
+import numpy as np
+import cv2
+
+
+inputPath = 'static/lion.png'
+
+
+orignalImage = cv2.imread(inputPath)
+
+
+# ------------Convert image to Grayscale --------------
+
+grayscaleImage = cv2.cvtColor(orignalImage, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('Grayscale Image', grayscaleImage)
+cv2.waitKey(0)
+
+
+# ----------------- Convert image to Oil Painting -----------------------
+
+# Apply the oil painting effect
+oilImg = cv2.xphoto.oilPainting(orignalImage, size=7, dynRatio=1)
+
+# Display the converted image
+cv2.imshow('Oil Paint Image', oilImg)
+cv2.waitKey(0)
