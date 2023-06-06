8860d59b636c1298f365c8b5f79de9da6fbe4961
vishalgddam873 <vishal18@navgurukul.org>
2023-06-06 12:41:15 +0530

    Code  updated

--- AA1/main.py
+++ AA1/main.py
@@ -6,12 +6,12 @@ import cv2
 inputPath = 'static/img1.png'
 
 # Load the color image
-orignalImage = cv2.imread(inputPath)
+originalImage = cv2.imread(inputPath)
 
 # ------------Rotate the image --------------
 
 # Rotate the image
-rotatedImage = cv2.rotate(orignalImage, cv2.ROTATE_90_CLOCKWISE)
+rotatedImage = cv2.rotate(originalImage, cv2.ROTATE_90_CLOCKWISE)
 
 # Save the image to disk
 outputPath = 'converted/rotated.png'
--- SA2_2/main.py
+++ SA2_2/main.py
@@ -6,12 +6,12 @@ import cv2
 inputPath = 'static/lion.png'
 
 
-orignalImage = cv2.imread(inputPath)
+originalImage = cv2.imread(inputPath)
 
 
 # ------------Convert image to Grayscale --------------
 
-grayscaleImage = cv2.cvtColor(orignalImage, cv2.COLOR_BGR2GRAY)
+grayscaleImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
 
 cv2.imshow('Grayscale Image', grayscaleImage)
 cv2.waitKey(0)
@@ -20,7 +20,7 @@ cv2.waitKey(0)
 # ----------------- Convert image to Oil Painting -----------------------
 
 # Apply the oil painting effect
-oilImg = cv2.xphoto.oilPainting(orignalImage, size=7, dynRatio=1)
+oilImg = cv2.xphoto.oilPainting(originalImage, size=7, dynRatio=1)
 
 # Display the converted image
 cv2.imshow('Oil Paint Image', oilImg)
